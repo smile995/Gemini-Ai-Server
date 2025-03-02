54ea217afe66ef59b920c38bbc65b1b05442875f
smile995 <ah.smile995@gmail.com>
2025-03-02 13:03:22 +0600

    Implement gemini ai to generate text to text

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = 5000;
+require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.Gemini_API);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+app.get("/gemini-prompt", async (req, res) => {
+  const { prompt } = req.body;
+  const result = await model.generateContent(prompt);
+  res.send(result.response.text());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
